13072fcaef9657c86b955256a71ab74ac79d9e5a
Halbow <alexis_huberlant@hotmail.com>
2023-11-13 07:32:59 +0100

    Leftover

--- src/atomik/_atomik.py
+++ src/atomik/_atomik.py
@@ -6,16 +6,15 @@ from pathlib import Path
 from typing import Iterator
 
 
-
 @contextlib.contextmanager
 def file(file_name: str | Path) -> Iterator[typing.IO]:
     file_name = file_name if isinstance(file_name, Path) else Path(file_name)
     fd, name = tempfile.mkstemp()
-    print(name)
     f = os.fdopen(fd, "wt")
     yield f
     f.close()
     os.rename(name, file_name)
+
 
 @contextlib.contextmanager
 def folder(file_name: str | Path) -> Path:
