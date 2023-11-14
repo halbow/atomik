5599321571f3b534bef855fb3ee260a08fed7992
Alexis Huberlant <alexis.huberlant@cluepoints.com>
2023-11-14 08:27:55 +0100

    Fix Flags

--- src/atomik/_rename_linux.py
+++ src/atomik/_rename_linux.py
@@ -9,8 +9,8 @@ libc = ctypes.CDLL("libc.so.6", use_errno=True)
 
 
 RENAME = 0
-RENAME_EXCHANGE = 1 << 1
-RENAME_NOREPLACE = 1 << 2  # Not sure of the values ?
+RENAME_EXCHANGE = 2
+RENAME_NOREPLACE = 1
 
 FILE_EXIST = 17
 INVALID_CROSS_DEVICE = 18
@@ -20,9 +20,6 @@ AT_FDCWD = -100
 
 
 def _rename(src_path: str, dst_path: str, overwrite=False):
-    print(os.stat(src_path).st_dev)
-    print(os.stat("./tests/TEST_DATA/").st_dev)
-
     code = libc.renameat2(
         AT_FDCWD,
         fsencode(src_path),
@@ -39,5 +36,7 @@ def _rename(src_path: str, dst_path: str, overwrite=False):
         if errno == INVALID_CROSS_DEVICE:
             src_dev = os.stat(src_path).st_dev
             dst_dev = os.stat(Path(dst_path).parent).st_dev
-            raise InvalidCrossDeviceError(f"{strerror(errno)} '{src_path}' -> '{dst_path}' ({src_dev} != {dst_dev})")
+            raise InvalidCrossDeviceError(
+                f"{strerror(errno)} '{src_path}' -> '{dst_path}' ({src_dev} != {dst_dev})"
+            )
         raise AtomikError(f"Error during rename: {strerror(errno)}({errno})")
